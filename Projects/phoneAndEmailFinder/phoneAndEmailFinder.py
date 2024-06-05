3228a03d45ef65d9e92f798345a58d6e1acafdb7
Julio Gaekel <juliogaekel@gmail.com>
2024-06-05 12:23:14 +0200

    FIX mismatching error emailRegex
    Deleted space character within {} for digits in email address. Space between the min and max values create a mismatch problem. Must avoid using space characters within braces.

--- Projects/phoneAndEmailFinder/phoneAndEmailFinder.py
+++ Projects/phoneAndEmailFinder/phoneAndEmailFinder.py
@@ -5,20 +5,20 @@ import pyperclip, re
 
 # Create phone regex
 phoneRegex = re.compile(r'''(
-                        (\D{3}|\(\d{3}\))?                  # area code
+                        (\d{3}|\(\d{3}\))?                  # area code
                         (\s|-|\.)?                          # separator
                         (\d{3})                             # first 3 digits
                         (\s|-|\.)?                          # separator
                         (\d{4})                             # las 4 digits
-                        (\s*(ext|x|ext.)\s*(\d{2, 5}))?     # extension 
-                        )'''. re.VERBOSE)
+                        (\s*(ext|x|ext.)\s*(\d{2,5}))?      # extension 
+                        )''', re.VERBOSE)
 
 # Create email regex
 emailRegex = re.compile(r'''(
-                        [a-zA-Z0-9._%+-]+                      # username
-                        @                                      # @ symbol
-                        [a-zA-Z0-9._%+-]+                      # domain name
-                        (\.[a-zA-Z]{2, 4})                     # dot-something
+                        [a-zA-Z0-9._%+-]+                   # username
+                        @                                   # @ symbol
+                        [a-zA-Z0-9._%+-]+                   # domain name
+                        (\.[a-zA-Z]{2,4})                   # dot-something
                         )''', re.VERBOSE)
 
 # Find matches in clipboard text
