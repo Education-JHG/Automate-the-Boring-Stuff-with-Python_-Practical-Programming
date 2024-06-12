defba7c3f919d35c3171e24a20a04664fdbf982d
Julio Gaekel <juliogaekel@gmail.com>
2024-06-12 10:24:13 +0200

    Add pyperclip module
    - Allows program to use addresses in users clipboard.
    - Add comments

--- Projects/mapIt/mapIt.py
+++ Projects/mapIt/mapIt.py
@@ -1,10 +1,13 @@
 #! python3
 # mapIt - Launches a map in the browser using an address from the command line or clipboard.
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1: # Stores a list of the program's filename and command line argument. If this list has more than just the filename in it, then it evaluates to an integer greater than 1, meaning that command line arguments have been provided.
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
 
-# TODO: Get address from clipboard
+webbrowser.open('https://www.google.com/maps/place/' + address)
